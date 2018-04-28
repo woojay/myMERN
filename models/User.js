423f71ffe4d5b2b962a7471d9b54b803f67f0900
woojay <fullyanalog-programmer@yahoo.com>
2018-04-28 22:48:23 +0200

    + /users/register adds records to DB

--- models/User.js
+++ models/User.js
@@ -1,28 +1,28 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
 // Create Schema
 const UserSchema = new Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
-    required: true
+    required: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   avatar: {
     type: String,
-    required: true
   },
   date: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
 });
 
-module.exports  = User = mongoose.model('users', UserSchema);
+module.exports = User = mongoose.model('users', UserSchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,12 +2,60 @@ const express = require('express');
 
 const router = express.Router();
 
+// Bcrypt
+const bcrypt = require('bcryptjs');
+
+// User Model
+const User = require('../../models/User');
+
+// Gravatar
+const gravatar = require('gravatar');
+
 // @route  GET api/users/test
 // @desc   Test users route
 // @access Private
 router.get('/test', (req, res) => {
   res.json({
     msg: 'Users/test works',
+  });
+});
+
+// @route  GET api/users/register
+// @desc   register users
+// @access Public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email }).then((user) => {
+    // User exists
+    if (user) {
+      return res.status(400).json({
+        email: 'Email already exists',
+      });
+    }
+    const avatar = gravatar.url(req.body.email, {
+      s: '200', // size
+      r: 'pg', // Rating
+      d: 'mm', // Default
+    });
+
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      avatar,
+      password: req.body.password,
+    });
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser
+          .save()
+          .then((user) => {
+            res.json(user);
+          })
+          .catch(err => console.log(err));
+      });
+    });
   });
 });
 
