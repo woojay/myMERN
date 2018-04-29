035359dc67434ffe14d1ba5263b5f4aa5cd12fa6
woojay <fullyanalog-programmer@yahoo.com>
2018-04-29 16:25:06 +0200

    + DELETE /profile for PROFILE and USER purge

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -279,5 +279,18 @@ router.delete('/education/:edu_id', passport.authenticate('jwt', { session: fals
     .catch(err => res.status(404).json(err));
 });
 
+// @route  DELETE api/profile
+// @desc   Delete User and Profile
+// @access Private
+router.delete('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+  Profile.findOneAndRemove({ user: req.user.id })
+    .then(() => {
+      User.findOneAndRemove({ _id: req.user.id })
+        .then(() => res.json({ success: true }))
+        .catch(err => res.status(404).json(err));
+    })
+    .catch(err => res.status(404).json(err));
+});
 
 module.exports = router;
