f520997bf4645d03ff9c09f067b5c13fa0eeb2be
woojay <fullyanalog-programmer@yahoo.com>
2018-05-23 21:30:30 +0200

    + Add Experience to profile.  ^56

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -38,6 +38,19 @@ export const getCurrentProfile = () => dispatch => {
     );
 };
 
+// Add experience
+export const addExperience = (expData, history) => dispatch => {
+  axios
+    .post('/api/profile/experience', expData)
+    .then(res => history.push('/dashboard'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 // Delete Account and Profile
 export const deleteAccount = () => dispatch => {
   if (window.confirm('Are you sure?  This CANNOT be undone!')) {
--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -4,7 +4,7 @@ import TextFieldGroup from "../common/TextFieldGroup";
 import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-
+import { addExperience } from '../../actions/profileActions';
 
 class AddExperience extends Component {
 
@@ -27,13 +27,31 @@ class AddExperience extends Component {
     this.onCheck = this.onCheck.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   onSubmit(e) {
     e.preventDefault();
+
+    const expData = {
+      company: this.state.company,
+      title: this.state.title,
+      location: this.state.location,
+      from: this.state.from,
+      to: this.state.to,
+      current: this.state.current,
+      description: this.state.description,
+    }
+
+    this.props.addExperience(expData, this.props.history);
 
   }
 
   onChange(e) {
-    this.setState = ({
+    this.setState({
       [e.target.name]: e.target.value
     });
   }
@@ -147,6 +165,7 @@ class AddExperience extends Component {
 }
 
 AddExperience.propTypes = {
+  addExperience: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired,
   errors: PropTypes.object.isRequired
 }
@@ -156,4 +175,4 @@ const mapStateToProps = state => ({
   errors: state.errors
 });
 
-export default connect(mapStateToProps)(withRouter(AddExperience));
+export default connect(mapStateToProps, { addExperience })(withRouter(AddExperience));
