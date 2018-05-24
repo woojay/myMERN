fed4c21403c209981f68566779e48dfd8c8994b3
woojay <fullyanalog-programmer@yahoo.com>
2018-05-24 10:21:02 +0200

    ~ User Profiles view. ~60

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { connect } from
+  class Profiles extends Component {
+    render() {
+      return (
+        <div>
+
+        </div>
+      )
+    }
+  }
